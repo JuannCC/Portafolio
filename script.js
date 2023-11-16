39137add0b9c99e9710202d03a22dd0d03db0d97
JuannCC <90645839+JuannCC@users.noreply.github.com>
2023-11-16 11:05:42 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// script.js
+
+// Cambia el texto del párrafo en el encabezado
+document.getElementById("demo").innerHTML = "Geólogo apasionado por la exploración y análisis de recursos geológicos.";
