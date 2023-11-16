9bfb8077cd0d015b3d52583e0f908b708404b6a6
JuannCC <90645839+JuannCC@users.noreply.github.com>
2023-11-16 11:11:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
 // script.js
 
-// Cambia el texto del párrafo en el encabezado
-document.getElementById("demo").innerHTML = "Geólogo apasionado por la exploración y análisis de recursos geológicos.";
+// Cambia el color de fondo de la sección de papers al hacer clic en el botón
+function cambiarColorFondo() {
+    var papersSection = document.getElementById("papers");
+    papersSection.style.backgroundColor = getRandomColor();
+}
+
+// Genera un color hexadecimal aleatorio
+function getRandomColor() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// Agrega efecto de desplazamiento suave a los enlaces del menú
+document.addEventListener("DOMContentLoaded", function () {
+    var links = document.querySelectorAll('a[href^="#"]');
+    for (var link of links) {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+            var targetId = this.getAttribute("href").substring(1);
+            var targetElement = document.getElementById(targetId);
+            scrollToTarget(targetElement);
+        });
+    }
+});
+
+// Desplazamiento suave hacia el elemento objetivo
+function scrollToTarget(targetElement) {
+    window.scroll({
+        behavior: "smooth",
+        left: 0,
+        top: targetElement.offsetTop - 20 // Ajusta según sea necesario
+    });
+}
+
+//En este script, la función cambiarColorFondo cambia el color de fondo de la sección de papers al hacer clic en un botón. La función getRandomColor genera un color hexadecimal aleatorio.
+//Además, hay un código que agrega un efecto de desplazamiento suave a los enlaces del menú. Al hacer clic en un enlace, el documento se desplazará suavemente hasta la sección correspondiente.
